842d8f5129c54ff022416c5c6bacbb35697fe005
Custom coder <77490153+customcoder245@users.noreply.github.com>
2026-09-21 15:24:20 +0530

    Update server.py

--- amazon_mcp/server.py
+++ amazon_mcp/server.py
@@ -131,7 +131,11 @@ async def _lifespan(app):
 
 _mcp_host = os.environ.get("FASTMCP_HOST", os.environ.get("AMAZON_MCP_HOST", "127.0.0.1"))
 _mcp_port = int(os.environ.get("FASTMCP_PORT", os.environ.get("AMAZON_MCP_PORT", "8780")))
-mcp = FastMCP("amazon-sp",lifespan=_lifespan,host=_mcp_host,port=_mcp_port,transport_security=TransportSecuritySettings(allowed_hosts=["amazon-mcp-new.onrender.com","localhost",  "127.0.0.1" ,"omre-asc-mcp.fly.dev"] )
+mcp = FastMCP("amazon-sp",lifespan=_lifespan,host=_mcp_host,port=_mcp_port,transport_security=TransportSecuritySettings(allowed_hosts=["amazon-mcp-new.onrender.com","localhost",  "127.0.0.1" ,"omre-asc-mcp.fly.dev"], allowed_origins=[
+            "https://omre-asc-mcp.fly.dev",
+            "http://localhost:6274",
+            "http://127.0.0.1:6274",
+        ] )
 )
 install_mcp_api_key_middleware(mcp)
 install_ip_allowlist_middleware(mcp)
